a08166d78c04c6f0f01f98b2155b89958aecaae9
Urdur <urg3@hi.is>
2018-11-26 23:45:24 +0000

    Dæmatímavinna

---
+++ src/lib/lecture.js
@@ -0,0 +1,29 @@
+export default class Lecture {
+  constructor() {
+    this.container = document.querySelector('.lecture');
+    this.url = '../lectures.json';
+  }
+
+  loadLecture(slug) {
+    return fetch(this.url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Gat ekki sótt fyrirlestra');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        const found = data.lectures.find(i => i.slug === slug);
+        if (!found) {
+          throw new Error('Fyrirlestur fannst ekki');
+        }
+        return found;
+      });
+  }
+
+  load() {
+    const qs = new URLSearchParams(window.localStorage.search);
+    const slug = qs.get('slug');
+    this.loadLecture(slug).then(data => console.log(data));
+  }
+}
