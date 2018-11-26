82872b0c170982b24e6cbc0fb2681ccd3f7156ef
Urdur <urg3@hi.is>
2018-11-26 22:48:51 +0000

    Vinna úr dæmatíma

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,5 +1,13 @@
-export default function empty(element) {
+export function empty(element) {
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
+}
+
+export function createElement(el, text) {
+  const element = document.createElement(el);
+  if (text) {
+    element.appendChild(document.createTextNode(text));
+  }
+  return element;
 }
