653501b2a2fe13494fd5749b9c35305e2c416467
Urdur <urg3@hi.is>
2018-11-27 16:22:07 +0000

    Byrjunarvinna í að birta gögn

--- src/lib/lecture.js
+++ src/lib/lecture.js
@@ -1,30 +1,39 @@
 export default class Lecture {
   constructor() {
     this.container = document.querySelector('.lecture');
-    this.url = './lectures.json';
+    this.url = '../lectures.json';
   }
 
-  loadLecture(slug) {
-    console.log(fetch(this.url));
+  loadLecture(inputSlug) {
     return fetch(this.url)
       .then((res) => {
         if (!res.ok) {
           throw new Error('Gat ekki sótt fyrirlestra');
         }
-        return JSON.parse(res);
+        return res.json();
       })
       .then((data) => {
-        const found = data.lectures.find(i => i.slug === slug);
+        console.log(data.lectures);
+        const found = data.lectures.find(lecture => lecture.slug === inputSlug);
         if (!found) {
           throw new Error('Fyrirlestur fannst ekki');
         }
+        console.log(found);
         return found;
       });
   }
 
+  renderData(data) {
+    //
+  }
+
   load() {
     const qs = new URLSearchParams(window.localStorage.search);
+    console.log(window.localStorage.search);
     const slug = qs.get('slug');
-    this.loadLecture(slug).then(data => console.log(data));
+    console.log(slug);
+    // Virkar ekki núna fyrir hvaða fyrirlestur sem er!!
+    // Byrja á að fá gögn til að birtast!!
+    this.loadLecture("html-sagan").then(data => renderData(data));
   }
 }
