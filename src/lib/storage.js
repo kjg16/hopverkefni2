152aa5decb4b9c4b4abe58420af2b5aa30934a05
Urdur <urg3@hi.is>
2018-11-28 17:12:33 +0000

    LocalStorage virkni komin á

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -1,7 +1,18 @@
+export const LOCALSTORAGE_KEY = 'fin_lectures';
+
 export function loadSavedLectures() {
-  //
+  const finJSON = localStorage.getItem(LOCALSTORAGE_KEY);
+  const fin = JSON.parse(finJSON) || [];
+  return fin;
 }
 
 export function saveLectures(slug) {
-  console.log(slug);
+  const fin = loadSavedLectures();
+  const index = fin.indexOf(slug);
+  if (index >= 0) {
+    fin.splice(index, 1);
+  } else {
+    fin.push(slug);
+  }
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(fin));
 }
